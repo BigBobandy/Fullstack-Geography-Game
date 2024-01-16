d5e4d0bb0014cdb448f688e41e7779764b2f479f
BigBobandy <jdykes207@gmail.com>
2024-01-15 20:36:47 -0500

    added function to handle graceful shutdowns of database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,12 +5,26 @@ const connectDB = async () => {
     await mongoose.connect(process.env.ATLAS_URL);
 
     console.log("MongoDB Connected...");
+
+    // ensure graceful shutdown of mongoose on Signal Interrupt
+    // this is used more in development
+    process.on("SIGINT", gracefulShutdown);
+    // ensure graceful shutdown of mongoose on Signal Terminate
+    // this is used more in production
+    process.on("SIGTERM", gracefulShutdown);
   } catch (err) {
     console.error(err.message);
     // Node.js method that terminates the Node.js process immediately
     // thought process is: if my application can't connect to the database there's no point in continuing to run the application
     process.exit(1);
   }
+};
+
+const gracefulShutdown = () => {
+  mongoose.connection.close(() => {
+    console.log("Mongoose connection disconnected due to app termination");
+    process.exit(0);
+  });
 };
 
 module.exports = connectDB;
