1210bfdf00dbf799a315c88a5d6bb6bccf36a164
Jackson <125409200+BigBobandy@users.noreply.github.com>
2024-01-27 16:51:46 -0500

    8 setup basic homepage (#11)
    * changed website title, added globe favicon
    
    * basic homepage setup
    
    * basic login page setup
    
    * fixed react router and imported home and login pages, created routes for both

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main className="bg-neutral-content border-solid border-2 border-primary-content rounded-md min-h-[90vh] my-4 w-full p-4">
+      <h1 className="font-bold">Geography-Genius</h1>
+      <p className="text-lg">A Geography Quiz App</p>
+    </main>
+  );
+}
+
+export default Home;
