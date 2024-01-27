f08859fe14f040ad8092c3022167374aab39470c
BigBobandy <jdykes207@gmail.com>
2024-01-27 16:50:25 -0500

    basic login page setup

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page</h1>
+      <button>Login with Google</button>
+      <button>Login with GitHub</button>
+      <button>Login with Facebook</button>
+      <button>Login with Twitter</button>
+    </div>
+  );
+};
+
+export default Login;
