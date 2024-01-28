d5a49263eb98b7ef793c0f3a4dca02361bdcf88f
Jackson <125409200+BigBobandy@users.noreply.github.com>
2024-01-28 11:38:17 -0500

    14 setup react redux (#15)
    * Update app name in README.md
    
    * Update dependencies: react-redux and redux
    
    * Update padding in App.css
    
    * Add store configuration to frontend/src/store/store.js
    
    * Add Redux store provider to main.jsx

---
+++ frontend/src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: process.env.NODE_ENV !== "production", // uses redux devtools in development
+});
+
+export default store;
