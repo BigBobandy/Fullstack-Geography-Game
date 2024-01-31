41b4a7006f19d41b6462be42c57987dbb5e053f4
BigBobandy <jdykes207@gmail.com>
2024-01-31 17:38:54 -0500

    Add redux hooks for login process

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,12 +1,18 @@
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loginStart } from "../store/slices/authSlice";
 import googleClientId from "../utils/config";
 
 const LoginButton = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.auth.isLoading);
+
   const redirectUri = "http://localhost:3000/auth/google/redirect";
 
   function handleLoginWithGoogle() {
+    dispatch(loginStart());
     window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=${encodeURIComponent(
       redirectUri
     )}&client_id=${googleClientId}&scope=profile email`;
@@ -14,10 +20,19 @@ const LoginButton = () => {
 
   return (
     <button
-      className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg font-bold"
+      class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg text-lg"
       onClick={handleLoginWithGoogle}
     >
-      Login with Google
+      {isLoading ? (
+        <span className="loading loading-infinity loading-md"></span>
+      ) : (
+        <>
+          Login with Google{" "}
+          <span>
+            <FontAwesomeIcon icon={faGoogle} />
+          </span>
+        </>
+      )}
     </button>
   );
 };
