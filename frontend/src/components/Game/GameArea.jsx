2b034e06866545df4549b269ccfa208a1e0b72dc
BigBobandy <jdykes207@gmail.com>
2024-02-02 18:41:05 -0500

    Add title and empty space to GameArea component

--- frontend/src/components/Game/GameArea.jsx
+++ frontend/src/components/Game/GameArea.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const GameArea = () => {
-  return <div>GameArea</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-xl">Can you guess what country this is?</h1>
+      <div className="h-[30vh]"></div>
+    </div>
+  );
 };
 
 export default GameArea;
