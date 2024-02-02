1dc7b0a74c7014996d21e1c9b3920b65308d4670
BigBobandy <jdykes207@gmail.com>
2024-02-02 16:54:33 -0500

    Add new screens and spacing options to tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xl: "1280px",
+      },
+      spacing: {
+        100: "25rem",
+      },
+    },
   },
+
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["retro", "forest"],
