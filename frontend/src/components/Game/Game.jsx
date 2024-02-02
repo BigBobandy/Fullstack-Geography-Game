e1adcc3f5b85e9ab036e45dfaba392568de6ee30
BigBobandy <jdykes207@gmail.com>
2024-02-02 17:51:16 -0500

    Add Game component to Home page and refactored styles

---
+++ frontend/src/components/Game/Game.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Game = () => {
+  return (
+    <div className="min-h-[90vh] my-4 p-4 bg-neutral-content border-2 border-primary-content rounded-md lg:w-[30vw] lg:mx-auto md:w-1/2 md:mx-auto">
+      Game
+    </div>
+  );
+};
+
+export default Game;
--- frontend/src/components/Pages/Home.jsx
+++ frontend/src/components/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Game from "../Game/Game";
 import Header from "../UI/Header";
 
 function Home() {
@@ -10,12 +11,9 @@ function Home() {
   }
 
   return (
-    <main className=" items-center w-3/4 h-3/4">
-      <div className="bg-neutral-content border-solid border-2 border-primary-content rounded-md min-h-[90vh] my-4 w-full p-4">
-        <Header />
-        <h1 className="font-bold">Geography-Genius</h1>
-        <p className="text-lg">Welcome, {user.displayName}</p>
-      </div>
+    <main className=" ">
+      <Header />
+      <Game />
     </main>
   );
 }
