ce0cdfff19aa7f76a5dce30c460b94cbf42b7045
BigBobandy <jdykes207@gmail.com>
2024-02-02 17:50:59 -0500

    Update Header component styling

--- frontend/src/components/UI/Header.jsx
+++ frontend/src/components/UI/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   const user = useSelector((state) => state.auth.user);
 
   return (
-    <header className="flex items-center justify-between bg-neutral-content py-1 text-neutral rounded-md mx-auto max-w-4xl">
+    <header className="flex items-center justify-between bg-neutral-content py-1 text-neutral rounded-md lg:w-[30vw] lg:mx-auto md:w-1/2 md:mx-auto min-w-fit">
       <div className="flex items-center">
         <h1 className="text-md font-bold flex items-center">
           Geography
@@ -18,15 +18,15 @@ const Header = () => {
         </h1>
       </div>
 
-      <div className="flex justify-center gap-3">
-        <button className="btn btn-xs sm:btn-sm  ">
+      <div className="flex justify-center sm:gap-2 md:gap-3 lg:gap-4">
+        <button className="btn btn-xs sm:btn-sm">
           <FontAwesomeIcon icon={faQuestionCircle} />
         </button>
-        <button className="btn btn-xs sm:btn-sm  ">
+        <button className="btn btn-xs sm:btn-sm">
           Leaderboard
           <FontAwesomeIcon icon={faRankingStar} />
         </button>
-        <button className="btn btn-xs sm:btn-sm  ">
+        <button className="btn btn-xs sm:btn-sm">
           Stats
           <FontAwesomeIcon icon={faStar} />
         </button>
