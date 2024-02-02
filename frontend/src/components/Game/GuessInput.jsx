bb11f0e84a0c4ae689b509618cfde605568831b0
Jackson <125409200+BigBobandy@users.noreply.github.com>
2024-02-02 18:42:33 -0500

    Task/style game components (#47)
    * Refactor Game component structure
    
    * Add title and empty space to GameArea component
    
    * Add GuessButton component
    
    * Add input field and submit functionality to GuessInput component
    
    * Refactor GuessList component to display a list of rounded rectangles
    
    * Update Home component with overflow-hidden class

--- frontend/src/components/Game/GuessInput.jsx
+++ frontend/src/components/Game/GuessInput.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import GuessButton from "./GuessButton";
 
 const GuessInput = () => {
-  return <div>GuessInput</div>;
+  const [guess, setGuess] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(guess);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className=" flex flex-row justify-between mt-2 w-full"
+    >
+      <input
+        type="text"
+        value={guess}
+        placeholder="Guess a country..."
+        className="input input-bordered w-full"
+        onChange={(event) => setGuess(event.target.value)}
+      />
+      <GuessButton />
+    </form>
+  );
 };
 
 export default GuessInput;
