b661df7bef4a881935481cd7668d72155fd3ce00
Jackson <125409200+BigBobandy@users.noreply.github.com>
2024-02-05 17:44:37 -0500

    Task/define schemas (#59)
    * Add dailyChallengeSchema model
    
    * Add userStatsSchema model

---
+++ backend/models/dailyChallengeSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const dailyChallengeSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    dailyCountry: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Country",
+      required: true,
+    },
+    guesses: [
+      {
+        guessNum: Number, // 1-6
+        guess: String, // Country name
+        isCorrect: Boolean, // true or false
+        hintsUsed: Number, // 0-3
+        distance: Number, // in km
+        direction: String, // N, NE, E, SE, S, SW, W, NW
+      },
+    ],
+    challengeDate: {
+      type: Date,
+      required: true,
+    },
+    isComplete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("DailyChallenge", dailyChallengeSchema);
