5a0d6d1985acb8b42c44f22e388a6f8abfc655c2
BigBobandy <jdykes207@gmail.com>
2024-02-05 08:54:23 -0500

    Update game layout and styling

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -7,7 +7,7 @@ const Game = () => {
   return (
     <div
       className="flex flex-col justify-between min-h-[90vh] my-4 p-4 bg-neutral-content border-2 border-primary-content 
-    rounded-md lg:w-[30vw] lg:mx-auto md:w-1/2 md:mx-auto"
+    rounded-md lg:w-[30vw] lg:mx-auto md:w-[75vw] md:mx-auto"
     >
       <GameArea />
       <GuessList />
