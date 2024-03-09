c05ca2fdb8121e940ec6ef7fac8727efff1804b8
Jackson <125409200+BigBobandy@users.noreply.github.com>
2024-03-09 18:10:20 -0500

    Task/set challenge logic (#61)
    * Add index for user and challengeDate in dailyChallengeModel.js
    
    * Add setDailyChallenge function to challengeService.js
    
    * Add setDailyChallenge function call in handleGameLogic

--- backend/game/controllers/gameController.js
+++ backend/game/controllers/gameController.js
@@ -1,4 +1,5 @@
 const selectCountry = require("../services/countryService");
+const setDailyChallenge = require("../services/challengeService");
 
 async function handleGameLogic() {
   try {
@@ -7,6 +8,13 @@ async function handleGameLogic() {
     console.log("Selected country: ", selectedCountry.name);
 
     // step 2 - use selected country to set the challenge in database
+    await setDailyChallenge(selectedCountry._id);
+
+    // step 3 - manage guesses and feedback
+
+    // step 4 - hint logic
+
+    // step 5 - end game logic
   } catch (err) {
     console.error("Error in game logic", err);
     throw err;
