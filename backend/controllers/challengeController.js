b37e3964176b1c9b7e1e4319aa211e15fea33d47
BigBobandy <jdykes207@gmail.com>
2024-03-11 11:51:39 -0400

    Refactor getDailyChallenge function to directly serve image file

--- backend/controllers/challengeController.js
+++ backend/controllers/challengeController.js
@@ -1,6 +1,5 @@
 const DailyChallenge = require("../models/dailyChallengeModel");
 const path = require("path");
-const fs = require("fs");
 
 async function getDailyChallenge(req, res) {
   const today = new Date();
@@ -17,22 +16,21 @@ async function getDailyChallenge(req, res) {
       // Construct the path to the image file
       const imagePath = path.join(
         __dirname,
-        "../assets",
+        "../",
         challenge.dailyCountry.outlineImageUrl
       );
 
-      try {
-        // Attempt to access the file to check its existence
-        await fs.access(imagePath);
-        // Serve the image file directly if the file is accessible
-        res.sendFile(imagePath);
-      } catch (err) {
-        // If the file is not accessible, respond with an error
-        res.status(404).json({ message: "Image not found" });
-      }
+      console.log("Attempting to serve imagePath: ", imagePath);
+      // Directly serve the image file
+      return res.sendFile(imagePath, (err) => {
+        if (err) {
+          console.error("Error serving file:", err);
+          return res.status(404).json({ message: "Image file not found." });
+        }
+      });
     }
   } catch (err) {
-    console.error(err);
+    console.error("Error fetching daily challenge:", err);
     res.status(500).json({ message: "Error getting daily challenge" });
   }
 }
