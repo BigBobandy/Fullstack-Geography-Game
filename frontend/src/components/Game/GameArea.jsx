e6132cc83e99d3d37875eff1c838ecc778bf54d3
BigBobandy <jdykes207@gmail.com>
2024-03-11 11:52:14 -0400

    Add daily challenge image fetching and display

--- frontend/src/components/Game/GameArea.jsx
+++ frontend/src/components/Game/GameArea.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDailyChallengeImage } from "../../store/slices/challengeSlice";
 
 const GameArea = () => {
+  const dispatch = useDispatch();
+  const { imageUrl, isLoading, isError, errorMessage } = useSelector(
+    (state) => state.challenge
+  );
+
+  useEffect(() => {
+    dispatch(fetchDailyChallengeImage());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1 className="font-bold text-xl">Loading...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1 className="font-bold text-xl">Error: {errorMessage}</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1 className="font-bold text-xl">Can you guess what country this is?</h1>
-      <div className="flex justify-center h-[55vh]"></div>
+      {imageUrl && (
+        <div className="flex justify-center h-[55vh]">
+          <img src={imageUrl} alt="Country Outline" />
+        </div>
+      )}
     </div>
   );
 };
