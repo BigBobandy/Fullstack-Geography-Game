cd440d70da7e435cb14335ffa929eed3d76febfd
BigBobandy <jdykes207@gmail.com>
2024-03-13 15:25:39 -0400

    Refactor Game component to use useState hook

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import { useState } from "react";
+import guessSlice from "../../store/slices/guessSlice";
 import GameArea from "./GameArea";
 import GuessInput from "./GuessInput";
 import GuessList from "./GuessList";
 
 const Game = () => {
+  const [totalGuessSlots, setTotalGuessSlots] = useState(6);
+
   return (
     <div
       className="flex flex-col justify-between min-h-[90vh] my-4 p-4 bg-neutral-content border-2 border-primary-content 
-    rounded-md lg:w-[30vw] lg:mx-auto md:w-[75vw] md:mx-auto"
+    rounded-md lg:w-[30vw]  lg:mx-auto md:w-[75vw] md:mx-auto"
     >
       <GameArea />
-      <GuessList />
-      <GuessInput />
+      <GuessList totalGuessSlots={totalGuessSlots} />
+      <GuessInput
+        totalGuessSlots={totalGuessSlots}
+        setTotalGuessSlots={setTotalGuessSlots}
+      />
     </div>
   );
 };
