38993a20e6e7bf6b55b22270b9339e7b4dabe3b8
BigBobandy <jdykes207@gmail.com>
2024-03-22 15:15:24 -0400

    Add guess distance, direction, and proximity to GuessItem component

--- frontend/src/components/Game/GuessItem.jsx
+++ frontend/src/components/Game/GuessItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { directionArrows } from "../../utils/directionMap";
 
 const GuessItem = ({ guess }) => {
   if (!guess) {
@@ -10,10 +11,21 @@ const GuessItem = ({ guess }) => {
   const guessCode = guess.guessCode;
   const guessText = guess.guess;
   const isCorrect = guess.isCorrect;
+  0;
+  const guessDistance = guess.distance;
+  const guessDirection = guess.direction;
+  const guessProximity = guess.proximityPercentage;
+
+  // Find the image file name from the directionArrows object
+  const arrowImage = guessDirection ? directionArrows[guessDirection] : null;
+
+  const imagePath = arrowImage
+    ? new URL(`../../assets/arrows/${arrowImage}`, import.meta.url).href
+    : "";
 
   return (
-    <div className="flex justify-between items-center flex-row bg-base-200 rounded-lg my-1 col-span-7 h-8">
-      <div className="flex justify-center items-center">
+    <div className="grid grid-cols-7 gap-1 text-center font-semibold">
+      <div className="bg-base-200 rounded-lg flex items-center h-8 col-span-3 animate-reveal pl-2">
         {guessFlag && (
           <img
             src={guessFlag}
@@ -25,12 +37,20 @@ const GuessItem = ({ guess }) => {
         {guessText && <span className="text-md">{guessText}</span>}
       </div>
 
-      <div>
-        {isCorrect ? (
-          <span className="font-bold mr-4 text-green-500">Correct!</span>
-        ) : (
-          <span className="font-bold mr-4 text-red-500">Incorrect</span>
+      <div className="bg-base-200 rounded-lg flex items-center justify-center h-8 col-span-2 animate-reveal">
+        {guessDistance && (
+          <span className="font-bold mr-4">{guessDistance} km</span>
         )}
+      </div>
+      <div className="bg-base-200 rounded-lg flex items-center justify-center h-8 col-span-1 animate-reveal">
+        {" "}
+        {arrowImage && (
+          <img src={imagePath} alt={guessDirection} className="h-8 w-10" />
+        )}
+      </div>
+
+      <div className="bg-base-200 rounded-lg flex items-center justify-center h-8 col-span-1 animate-reveal animate-pop">
+        <span className="font-bold text-blue-500">{guessProximity}%</span>
       </div>
     </div>
   );
