8a20f14c40196d000132677cc84452b3f51df4c0
BigBobandy <jdykes207@gmail.com>
2024-03-22 15:15:03 -0400

    Add directionMap.js file with direction arrows

---
+++ frontend/src/utils/directionMap.js
@@ -0,0 +1,10 @@
+export const directionArrows = {
+  N: "North.png",
+  NE: "North-East.png",
+  NW: "North-West.png",
+  W: "West.png",
+  SW: "South-West.png",
+  SE: "South-East.png",
+  S: "South.png",
+  E: "East.png",
+};
