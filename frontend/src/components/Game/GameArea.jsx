716eca10f7ba89e82b7d2fa945a68b4b142fc388
BigBobandy <jdykes207@gmail.com>
2024-03-23 15:20:47 -0400

    Refactor GameArea component to reset guess state when challenge ID changes

--- frontend/src/components/Game/GameArea.jsx
+++ frontend/src/components/Game/GameArea.jsx
@@ -8,27 +8,23 @@ import { resetGuessState } from "../../store/slices/guessSlice";
 
 const GameArea = () => {
   const dispatch = useDispatch();
-  const { imageUrl, isLoading, isError, errorMessage } = useSelector(
-    (state) => state.challenge
-  );
+  const {
+    imageUrl,
+    isLoading,
+    isError,
+    errorMessage,
+    challengeId,
+    previousChallengeId,
+  } = useSelector((state) => state.challenge);
 
   useEffect(() => {
     dispatch(fetchDailyChallengeImage());
-    dispatch(fetchDailyChallengeId()).then(({ payload }) => {
-      // Fetch the current and previous challenge IDs from the state
-      const currentChallengeId = useSelector(
-        (state) => state.challenge.challengeId
-      );
-      const previousChallengeId = useSelector(
-        (state) => state.challenge.previousChallengeId
-      );
-
-      // If the challenge ID has changed, reset the guess state
-      if (currentChallengeId !== previousChallengeId) {
+    dispatch(fetchDailyChallengeId()).then(() => {
+      if (challengeId !== previousChallengeId) {
         dispatch(resetGuessState());
       }
     });
-  }, [dispatch]);
+  }, [dispatch, challengeId, previousChallengeId]);
 
   if (isLoading) {
     return (
