055ad42a102a730f1e1aa0a70899dc576be00eae
BigBobandy <jdykes207@gmail.com>
2024-03-28 18:16:40 -0400

    Update redirect URL in authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,11 @@ const passport = require("passport");
 const router = express.Router();
 const { loginWithGoogle } = require("../controllers/authControllers");
 
+const clientUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.CLIENT_URL
+    : "http://localhost:5173";
+
 router.get("/google", loginWithGoogle);
 
 router.get(
@@ -10,7 +15,7 @@ router.get(
   passport.authenticate("google", { failureRedirect: "/login" }),
   (req, res) => {
     // Successful authentication, redirect back to the client application.
-    res.redirect("http://localhost:5173/auth/success");
+    res.redirect(`${clientUrl}/auth/success`);
   }
 );
 
