959a208f4cdc013db55629aacec1474d7efb577c
BigBobandy <jdykes207@gmail.com>
2024-03-31 20:38:56 -0400

    Add useEffect and useDispatch to Game component

--- frontend/src/components/Game/Game.jsx
+++ frontend/src/components/Game/Game.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGuesses } from "../../store/actions/guessActions.js";
+import {
+  fetchDailyChallengeId,
+  fetchDailyChallengeImages,
+} from "../../store/slices/challengeSlice.js";
 import GameArea from "./GameArea.jsx";
 import GuessInput from "./GuessInput.jsx";
 import GuessList from "./GuessList.jsx";
 
 const Game = () => {
   const [totalGuessSlots, setTotalGuessSlots] = useState(6);
+  const dispatch = useDispatch();
+  const { challengeId, previousChallengeId } = useSelector(
+    (state) => state.challenge
+  );
+
+  useEffect(() => {
+    dispatch(fetchDailyChallengeId())
+      .unwrap()
+      .then((newChallengeId) => {
+        dispatch(fetchDailyChallengeImages());
+        dispatch(getGuesses(newChallengeId.challengeId));
+      })
+      .catch((error) => console.error("Failed to fetch challenge ID:", error));
+  }, [dispatch]);
 
   return (
     <div
